d918bee5b285615a29313695ec23392952d5ed04
akjaya <putulakjaya@gmail.com>
2020-02-24 17:14:02 +0530

    firebase and comments added

---
+++ build/precache-manifest.e9fe48afbaf22ac0db8032f64bfe7b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f658a9d346e1596374a54acef9697bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b355f3233df8d27107ed",
+    "url": "/static/css/main.9e1acea3.chunk.css"
+  },
+  {
+    "revision": "a0bfac50b18a96d0250a",
+    "url": "/static/js/2.2638ff5d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2638ff5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b355f3233df8d27107ed",
+    "url": "/static/js/main.8d3d4cbd.chunk.js"
+  },
+  {
+    "revision": "91f1b01d15ac5771456c",
+    "url": "/static/js/runtime-main.51893e71.js"
+  }
+]);
